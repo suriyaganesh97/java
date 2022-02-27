0a7a6bc672741df8443ee16fbe90a9bfe0b98137
suriyaganesh97 <suriyaganesh97@gmail.com>
2022-02-27 20:00:40 +0530

    testing java

---
+++ New folder/TimJavabasics/src/testjava/tesfile.java
@@ -0,0 +1,9 @@
+package testjava;
+
+public class tesfile {
+	public static void main(String args[])
+	{
+		 System.out.println("Hello, World!"); 
+	}
+   
+}
---
+++ testjava/src/testjava/Firsttestfile.java
@@ -0,0 +1,10 @@
+package testjava;
+
+public class Firsttestfile {
+
+	public static void main(String[] args) {
+		System.out.println("Hello, World!"); 
+
+	}
+
+}
