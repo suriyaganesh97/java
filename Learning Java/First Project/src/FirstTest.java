5238e98c43f409c53b36ab3903c3c8db25a81ddd
suriyaganesh97 <suriyaganesh97@gmail.com>
2022-04-01 12:54:30 +0530

    learning java

---
+++ Learning Java/First Project/src/FirstTest.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class FirstTest {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter a number");
+        int a = sc.nextInt();
+        sc.close();
+        System.out.println("the entered number is "+a);
+    }
+}
