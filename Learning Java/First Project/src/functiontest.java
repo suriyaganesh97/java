f560395ca683347a7646cd8cc9f3c5d923ba57a2
suriyaganesh97 <suriyaganesh97@gmail.com>
2022-04-22 07:22:42 +0530

    normal function test

---
+++ Learning Java/First Project/src/functiontest.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class functiontest {
+    public static void main(String[] args){
+//        System.out.println("function working");
+        addnumbers(3,5);
+    }
+
+    private static void addnumbers(int num1, int num2) {
+        int sum = num1 + num2;
+//        System.out.println(sum);
+        for(int i=1;i<=10;i++){
+            System.out.println(i);
+        }
+    }
+
+
+}
