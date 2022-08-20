1bd46d6e160441882fa6f0b1a2447d9e30041651
suriyaganesh97 <suriyaganesh97@gmail.com>
2022-08-20 16:36:03 +0530

    adding new controller

--- Spring boot/firstWebApplication/src/main/java/com/suriya/first/controller/Controller.java
+++ Spring boot/firstWebApplication/src/main/java/com/suriya/first/controller/Controller.java
@@ -2,6 +2,7 @@ package com.suriya.first.controller;
 
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Controller {
@@ -18,5 +19,11 @@ public class Controller {
 	@RequestMapping("/jspTest")
 	public String jspTest() {
 		return "sayHello";
+}
+	
+	@RequestMapping("/login")
+	public String logIn(@RequestParam String name) {
+		System.out.println(name);
+		return "Login";
 }
 }
