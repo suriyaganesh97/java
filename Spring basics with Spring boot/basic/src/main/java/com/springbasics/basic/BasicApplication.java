4fc252208d6248610b564f77d7850034b8ce40d3
suriyaganesh97 <suriyaganesh97@gmail.com>
2022-09-10 18:44:33 +0530

    spring basics

---
+++ Spring basics with Spring boot/basic/src/main/java/com/springbasics/basic/BasicApplication.java
@@ -0,0 +1,16 @@
+package com.springbasics.basic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class BasicApplication {
+
+	public static void main(String[] args) {
+		ApplicationContext context =SpringApplication.run(BasicApplication.class, args);
+		User obj=context.getBean(User.class);
+		obj.printusername();
+	}
+
+}
---
+++ Spring basics with Spring boot/basic/src/main/java/com/springbasics/basic/User.java
@@ -0,0 +1,11 @@
+package com.springbasics.basic;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class User {
+public void printusername(){
+    System.out.println("suriya");
+   
+}
+}
