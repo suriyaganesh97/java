053769a3e6bb1b524f0c04b4952736006796a7f1
suriyaganesh97 <suriyaganesh97@gmail.com>
2023-03-01 06:38:53 +0530

    ip address and notepad open

---
+++ Random Java Programs/Notepad/Notepad.java
@@ -0,0 +1,14 @@
+package Notepad;
+
+import java.io.*;
+class Notepad {
+public static void main(String[] args) {
+Runtime rs = Runtime.getRuntime();
+try {
+rs.exec("notepad");
+}
+catch (IOException e) {
+System.out.println(e);
+}
+}
+}
---
+++ Random Java Programs/ipAddress/IpAddress.java
@@ -0,0 +1,9 @@
+package ipAddress;
+
+import java.net.InetAddress;
+
+class IpAddress{
+public static void main(String Args[]) throws Exception{
+System.out.println(InetAddress.getLocalHost());
+}
+}
